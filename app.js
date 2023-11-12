27a5431605e805f05870836f4a35f61b7503fb5e
chaoui135 <chaoui135>
2023-11-12 13:27:18 +0100

    comiit

---
+++ app.js
@@ -0,0 +1,65 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'movies_db',
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+// Routes
+
+// Get all movies
+app.get('/movies', (req, res) => {
+  // Implement logic to retrieve all movies from the database
+  res.json({ message: 'Get all movies' });
+});
+
+// Get a movie by ID
+app.get('/movies/:id', (req, res) => {
+  // Implement logic to retrieve a movie by ID from the database
+  const movieId = req.params.id;
+  res.json({ message: `Get movie with ID ${movieId}` });
+});
+
+// Create a new movie
+app.post('/movies', (req, res) => {
+  // Implement logic to create a new movie in the database
+  const movieData = req.body;
+  res.status(201).json({ message: 'Movie created successfully', movie: movieData });
+});
+
+// Update a movie by ID
+app.put('/movies/:id', (req, res) => {
+  // Implement logic to update a movie by ID in the database
+  const movieId = req.params.id;
+  const updatedData = req.body;
+  res.json({ message: `Update movie with ID ${movieId}`, updatedData });
+});
+
+// Delete a movie by ID
+app.delete('/movies/:id', (req, res) => {
+  // Implement logic to delete a movie by ID from the database
+  const movieId = req.params.id;
+  res.json({ message: `Delete movie with ID ${movieId}` });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- filmsdb.sql
+++ filmsdb.sql
@@ -1,9 +1,20 @@
-CREATE TABLE FILM BABYLON ( 
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Nom TEXT NOT NULL CHECK(length(Nom) <= 128),
-    Description TEXT NOT NULL CHECK(length(Description) <= 2048),
-    DateParution DATE NOT NULL,
-    Note INTEGER CHECK(Note >= 0 AND Note <= 5)
+CREATE TABLE movies ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL CHECK(length(Nom) <= 128),
+    dateParution TEXT NOT NULL CHECK(length(Description) <= 2048),
+    dateParution DATE NOT NULL,
+    note INTEGER CHECK(Note >= 0 AND Note <= 5)
 ); 
+
+INSERT INTO movies (id, nom, dateParution, dateParution, note)
+ VALUES
+ ('12345', 'Marvel', 'Film daction', 2023,5),
+ ('23456', 'Avatar', 'Film daventure', 2023,4);
+
+
+
+
+
+
 
 
