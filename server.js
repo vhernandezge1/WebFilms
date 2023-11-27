97ddfc663d460e04af9842611caf1a26f6362c5a
vhernandezge1 <kvele44@gmail.com>
2023-11-27 14:49:48 +0100

    suppr app.js

--- server.js
+++ server.js
@@ -1,29 +1,97 @@
+// app.js
 const express = require('express');
-const mysql = require('mysql2/promise');
-
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+ 
 const app = express();
-const port = 3000;
-
-// Configuration de la base de données
-const connection = mysql.createPool({
-    host: 'votre_hote',
-    user: 'votre_utilisateur',
-    password: 'votre_mot_de_passe',
-    database: 'votre_base_de_donnees'
+const port = 3306;
+ 
+app.use(bodyParser.json());
+ 
+const db = mysql.createConnection({
+  host: '3306',
+  user: 'valentin',
+  password: '1234',
+  database: 'movies',
 });
-
-// Route pour récupérer des données de la base de données
-app.get('/recuperer-donnees', async (req, res) => {
-    try {
-        const [rows] = await connection.query('SELECT * FROM votre_table');
-        res.json(rows);
-    } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error);
-        res.status(500).send('Erreur serveur');
+ 
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+ 
+ 
+ 
+ 
+// Get a movie by ID
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await Film.findAll();
+    res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'ressource absente' });
+  }
+});
+ 
+ 
+// Create a new movie
+app.post('/films', async (req, res) => {
+  const { id, nom, description, dateParution, note } = req.body;
+ 
+  try {
+    const newMovie = await Film.create({ nom, description, dateParution, note });
+    res.status(201).json(newMovie);
+  } catch (error) {
+    console.error(error);
+    res.status(422).json({ error: 'Validation impossible' });
+  }
+});
+ 
+ 
+// Update a movie by ID
+app.put('/films/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nom, description, dateParution, note } = req.body;
+ 
+  try {
+    const film = await Film.findByPk(id);
+ 
+    if (film) {
+      await film.update({ nom, description, dateParution, note });
+      res.status(200).json({ message: 'Film modifié avec succès.' });
+    } else {
+      res.status(404).json({ error: 'ressource absente' });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(422).json({ error: 'validation impossible' });
+  }
 });
-
-// Démarrer le serveur
+ 
+ 
+app.delete('/films/:id', async (req, res) => {
+  const { id } = req.params;
+ 
+  try {
+    const film = await Film.findByPk(id);
+ 
+    if (film) {
+      await film.destroy();
+      res.status(201).json({ message: 'Film supprimé avec succès.' });
+    } else {
+      res.status(404).json({ error: 'ressource absente' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(422).json({ error: 'validation impossible' });
+  }
+});
+ 
 app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+ 
